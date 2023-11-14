00e125ee50c01cafd3fe88568d419b723eea648a
Arjun Puri <arjunpur2@gmail.com>
2023-11-13 16:27:41 -0800

    Implement initial branching algorithm

--- plots/spacefill/sketch_spacefill.py
+++ plots/spacefill/sketch_spacefill.py
@@ -9,6 +9,18 @@ from typing import List
 from shapely import Point
 
 
+# 11/13
+# - Implemented a branching algorithm that selects arbitrary nodes in the existing set and starts branching at an angle perpendicular
+#   to where we are at now
+# - We're not getting the space filling that we want. We're getting a lot of empty space. Hopefully this will be fixed when the collition
+#   detection algorithm is in place.
+# # TODO: Vary the perturbation more as the branch level increases
+# # TODO: Vary the decrease in the radius more as the branch level increases
+# # TODO: Implement collision detection
+# # TODO: There may be a bug where when you create a perpendicular branch, the start of the branch is not from the center of the previous node
+#         by design and so you see a small space when we branch. This might be fixed if we select the right starting radius of the circles to be small enough
+#
+
 # 10/29
 # - Currently the space filling algorithm initializes nodes and then creates new nodes from those initial nodes
 #   by taking the direction of travel, perturbating it, and creating new nodes on the perimeter untill we reach the boundary
@@ -16,7 +28,6 @@ from shapely import Point
 # - Create a collision detection algorithm to ensure no new nodes overlap with any existing ones (attempt to implement this with matrix multiplication / vectorization)
 # - Implement a branching algorithm (reducing the width, increasing the pertubation range)
 # - Ensure that we are space filling well
-# TODO: There's a bug where the next_node's aren't completely non-intersecting (THE MATH IS WRONG!!!!!!!!!!!!!!)
 
 
 # Space Branch-Fill Algorithm
@@ -35,6 +46,10 @@ from shapely import Point
 #  - I can then use the distance formula to check if the distance between the centers of the circles is less than the sum of the radii
 
 class SpaceFillSketch(vsketch.SketchClass):
+
+    draw_circles = vsketch.Param(True)
+    draw_vector_tips = vsketch.Param(True)
+
     def draw(self, vsk: vsketch.Vsketch) -> None:
         vsk.size("9in", "11in", landscape=True, center=False)
         vsk.scale("in")
@@ -44,23 +59,35 @@ class SpaceFillSketch(vsketch.SketchClass):
 
         bfill = BranchFill(Circle(center.x, center.y, max_circle_radius))
         bfill.fill()
-        draw(vsk, bfill)
+        self.draw_bfill(vsk, bfill)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
 
+    def draw_bfill(self, vsk: vsketch.Vsketch, bfill: BranchFill):
+        boundary = bfill.boundary
+        vsk.circle(boundary.x, boundary.y, boundary.r * 2.0)
 
-def draw(vsk: vsketch.Vsketch, bfill: BranchFill):
-    boundary = bfill.boundary
-    vsk.circle(boundary.x, boundary.y, boundary.r * 2.0)
+        # for node in bfill.initial_nodes:
+        #     if self.draw_circles:
+        #         vsk.circle(node.circle.x, node.circle.y, node.circle.r * 2.0)
+        #     self.draw_node(vsk, node)
 
-    for node in bfill.initial_nodes:
-        vsk.circle(node.circle.x, node.circle.y, node.circle.r * 2.0)
-        draw_vector(vsk, node.direction, node.circle.center, draw_tip=True)
+        for node in bfill.all_nodes:
+            if self.draw_circles:
+                vsk.circle(node.circle.x, node.circle.y, node.circle.r * 2.0)
+            self.draw_node(vsk, node)
 
-    for node in bfill.all_nodes:
-        vsk.circle(node.circle.x, node.circle.y, node.circle.r * 2.0)
-        draw_vector(vsk, node.direction, node.circle.center, draw_tip=True)
+    def draw_node(self, vsk: vsketch.Vsketch, node: Node):
+        if self.draw_circles:
+            vsk.circle(node.circle.x, node.circle.y, node.circle.r * 2.0)
+
+        # Draw a single line segment in the direction of node.direction, passing
+        # through the center of the circle, of length equal to the diameter
+        # of the circle
+        reverse_direction = node.direction.normalize() * -1.0 * node.circle.r
+        start_point = add(node.circle.center, reverse_direction)
+        draw_vector(vsk, node.direction * 2.0, start_point, draw_tip=self.draw_vector_tips)
 
 
 class Circle:
@@ -120,10 +147,49 @@ class Circle:
 
 
 class Node:
-    def __init__(self, circle: Circle, direction: Vector):
+    def __init__(self, circle: Circle, direction: Vector, branch_level: int = 0):
         self.circle = circle
         self.direction = direction
+        self.branch_level = branch_level
 
+    def is_within(self, other: Circle) -> bool:
+        return self.circle.is_within(other)
+
+    @staticmethod
+    def from_previous_node(
+            previous_node: Node,
+            new_direction: Vector,
+            scale_radius_down_by: float = 1.0,
+            branch_level: int = 0,
+    ) -> Node:
+        """
+        Creates a new node from the previous node by pertubating the direction of the previous node
+        and scaling down the radius of the new node.
+        @param previous_node: The previous node to use as a reference to create the new node. We primarily use the previous
+        node's circle's center and radius to create the new node.
+        @param new_direction: The direction in which to create the new node.
+        @param scale_radius_down_by: How much to scale down the radius of the new node by. This is a value between 0 and 1.
+        @param start_from_perimeter: If True, then the new node's will begin from the perimeter of the previous node. If False,
+        then the new node will begin from previous node's center.
+        @param is_branch: If True, then the new node will have a branch_level of previous_node.branch_level + 1. If False,
+        """
+
+        # Using the current nodes center and direction, perturbate the angle a little bit
+        # and find a point on the perimeter of the circle. This will be the tangent point
+        # where the new node's circle will intersect with the current node.
+        #
+        # First perturbate to find the tangent point. We normalize the direction and scale it by the previous
+        # nodes radius to ensure we're on the perimeter of the circle
+        tangent_point = add(previous_node.circle.center, new_direction.normalize() * previous_node.circle.r)
+        starting_point = tangent_point
+
+        # Second, scale down the new radius, and find the new center
+        radius = previous_node.circle.r * scale_radius_down_by
+        scaled_direction = new_direction.normalize() * radius
+        new_center = add(starting_point, scaled_direction)
+        circle = Circle(new_center.x, new_center.y, radius)
+
+        return Node(circle, scaled_direction, branch_level)
 
 class AllCircles:
     def __init__(self):
@@ -141,19 +207,40 @@ class BranchFill:
         # This will allow us to use different shapes as the boundary.
         self.boundary = boundary
         self.initial_nodes = None
-        self.all_nodes = []
+        self.all_nodes: List[Node] = []
         self.NUM_INITIAL_NODES = 3
-        self.INITIAL_NODE_RADIUS_PCT = 0.1
-        self.NEXT_NODE_RADIUS_REDUCTION_PCT = 0.95
+        self.INITIAL_NODE_RADIUS_PCT = 0.01
+        self.NEXT_NODE_RADIUS_REDUCTION_PCT = 0.99
         self.NEXT_NODE_CROSS_BOUNDARY_RETRIES = 3
+        self.MIN_NODE_RADIUS_THRESHOLD_INCHES = 0.01
 
     def fill(self):
         self.initial_nodes = self.select_initial_nodes()
         for node in self.initial_nodes:
             initial_pertubation = (np.deg2rad(-15.0), np.deg2rad(15.0))
-            self.all_nodes.extend(self.begin_fill_from_node(node, initial_pertubation))
+            self.all_nodes.extend(self.begin_fill_from_node(node, node.direction, initial_pertubation))
+            self.all_nodes.append(node)
 
-    def begin_fill_from_node(self, node: Node, pertubation_range: (float, float)) -> List[Node]:
+        # Branching algorithm:
+        # Randomly pick nodes from self.all_nodes, pick an angle that's perpendicular (with some perturbation)
+        # to the direction of the original node's travel, and then continue filling but with a tighter radius,
+        # and a larger perturbation range.
+        NUM_BRANCH_STARTS = 80
+        for i in range(NUM_BRANCH_STARTS):
+            # Randomly pick a node, and a direction that's perpendicular to the node's direction. Perturbate the direction
+            # a bit
+            node = np.random.choice(self.all_nodes)
+            # Chose the direction to branch and create the branching node
+            clockwise = np.random.choice([True, False])
+            direction_to_branch = node.direction.normalize().get_perpendicular(clockwise=clockwise)
+
+            # Begin a fill from this node
+            self.all_nodes.extend(self.begin_fill_from_node(node, direction_to_branch, initial_pertubation))
+
+
+
+
+    def begin_fill_from_node(self, node: Node, initial_direction: Vector, pertubation_range: (float, float)) -> List[Node]:
         """
         Starts from a node's center and uses the direction to repeatedly create new nodes till it
         reaches the boundary and cannot create more nodes (with 3 retries of pertubations).
@@ -165,7 +252,9 @@ class BranchFill:
         @return:
         """
         new_nodes = []
+        next_branch_level = node.branch_level + 1
         current_node = node
+        current_direction = initial_direction
         retries = 0
         while True:
             # Using the current nodes center and direction, perturbate the angle a little bit
@@ -173,27 +262,25 @@ class BranchFill:
             # where the new node's circle will intersect with the current node.
             #
             # First perturbate to find the tangent point
-            direction_perturbated = current_node.direction.perturbate(pertubation_range)
-            tangent_point = add(current_node.circle.center, direction_perturbated)
-
-            # Second, scale down the new radius, and find the new center
-            radius = current_node.circle.r * self.NEXT_NODE_RADIUS_REDUCTION_PCT
-            new_direction = direction_perturbated.normalize() * radius
-            center = add(tangent_point, new_direction)
-            circle = Circle(center.x, center.y, radius)
+            direction_pertubated = current_direction.perturbate(pertubation_range)
+            new_node = Node.from_previous_node(current_node, direction_pertubated, scale_radius_down_by=self.NEXT_NODE_RADIUS_REDUCTION_PCT, branch_level=next_branch_level)
 
             # If the new circle is out of the boundary, allow for some retries with pertubations, but
             # eventually give up and return.
-            if not circle.is_within(self.boundary):
+            if not new_node.is_within(self.boundary):
                 if retries > self.NEXT_NODE_CROSS_BOUNDARY_RETRIES:
                     break
                 retries += 1
                 continue
 
+            # If the new node is too small, then give up
+            if new_node.circle.r < self.MIN_NODE_RADIUS_THRESHOLD_INCHES:
+                break
+
             # Register the new node
-            new_node = Node(circle, new_direction)
             new_nodes.append(new_node)
             current_node = new_node
+            current_direction = direction_pertubated
         return new_nodes
 
     def select_initial_nodes(self) -> List[Node]:
